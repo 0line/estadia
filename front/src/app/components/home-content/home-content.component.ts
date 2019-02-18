3d734b0045f05ad1bc784ea9a782706a15530f8d
0line <0line@users.noreply.github.com>
2019-02-18 08:32:17 -0600

    Producción hmtl

---
+++ front/src/app/components/home-content/home-content.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeSliderComponent } from '../home-slider/home-slider.component';
+
+@Component({
+  selector: 'app-home-content',
+  templateUrl: './home-content.component.html',
+  styleUrls: ['./home-content.component.scss']
+})
+export class HomeContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
